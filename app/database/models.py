b896229e88015a00dbf834875f85f2c191d7d49a
Abhinav <abhinavprajapati@gmail.com>
2024-10-08 23:48:08 +0530

    Refactor SensorData and Users classes in models.py

--- app/database/models.py
+++ app/database/models.py
@@ -1,19 +1,17 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+from sqlalchemy.orm import relationship
 
 from app.database.db import engine
 
 Base = declarative_base()
 
-''' NOTE: there is another class which uses same SensorData table make 
-    sure to make changes on both of them (if there is any update ) '''
-
 class SensorData(Base):
     __tablename__ = 'sensor_data_1'
     
     id = Column(Integer, primary_key=True, autoincrement=True)
-    timestamp = Column(DateTime, default=datetime.utcnow)  # Adding timestamp column
+    timestamp = Column(DateTime, default=datetime.utcnow)  
     sensor_id = Column(String(50), nullable=False)
     data = Column(String(255), nullable=False)
 
@@ -22,7 +20,7 @@ class Users(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
-    sensor_id = Column(String(50), nullable=False) # device which is assigned to that user
+    sensor_id = Column(String(50), nullable=False) 
 
 # Create the tables if not exists
 Base.metadata.create_all(bind=engine)
