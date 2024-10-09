92f95a9f63f2a8ae4a1b37ffd8f82511bc450c66
Abhinav <abhinavprajapati@gmail.com>
2024-10-09 19:56:44 +0530

    Add nullable=True to sensor_id and data columns, and add new columns for daily_goal, wakeup_time, sleep_time, and bottle_weight

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Time
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 from sqlalchemy.orm import relationship
@@ -12,8 +12,8 @@ class SensorData(Base):
     
     id = Column(Integer, primary_key=True, autoincrement=True)
     timestamp = Column(DateTime, default=datetime.utcnow)  
-    sensor_id = Column(String(50), nullable=False)
-    data = Column(String(255), nullable=False)
+    sensor_id = Column(String(50), nullable=True)
+    data = Column(String(255), nullable=True)
 
 class Users(Base):
     __tablename__ = 'users'
@@ -21,6 +21,11 @@ class Users(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
     sensor_id = Column(String(50), nullable=False) 
+
+    daily_goal = Column(Integer, nullable=True)
+    wakeup_time = Column(Time, nullable=True)
+    sleep_time = Column(Time, nullable=True) 
+    bottle_weight = Column(Integer, nullable=True) 
 
 # Create the tables if not exists
 Base.metadata.create_all(bind=engine)
