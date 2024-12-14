6a41e7b586f0877c169adaf0f920ba3b07867406
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2024-12-14 18:32:34 +0530

    make status bar transparent
    Make status bar transparent so that it take color or app making it
    appleing

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/context/authContext";
 import ProtectedRoute from "@/context/ProtectedRoute";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
+
   return (
     <AuthProvider>
       <ProtectedRoute>
+        <StatusBar backgroundColor="transparent" />
         <Stack>
           <Stack.Screen options={{ headerShown: false }} name="(tabs)" />
         </Stack>
