855dd3de8143d4cee47226b1f35a988952fda1ea
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2024-12-15 22:36:40 +0530

    fix jwt validator

--- backend/app/utils/validate_jwt.py
+++ backend/app/utils/validate_jwt.py
@@ -5,17 +5,18 @@ from jose import jwt, JWTError, ExpiredSignatureError
 from app.utils.config import PRIVATE_JWT_KEY
 
 ALGORITHM = "HS256"
-EXPECTED_AUDIENCE = "authenticated" 
+EXPECTED_AUDIENCE = "authenticated"
+
 
 def validate_jwt(token: str = Security(HTTPBearer())):
     try:
-        payload = jwt.decode(
+        decoded_token = jwt.decode(
             token.credentials,
             PRIVATE_JWT_KEY,
             algorithms=[ALGORITHM],
             audience=EXPECTED_AUDIENCE,
         )
-        return payload
+        return decoded_token["sub"]
     except ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired.")
     except JWTError as e:
