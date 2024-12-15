adffaa2d6a8f0e0d9b5b81c9bb86a5c4bc08b4eb
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2024-12-15 20:35:11 +0530

    use expo secure store to store users session creds
    this fixies the issue of jwt token not beeing saved at client side

--- frontend/utils/supabase.ts
+++ frontend/utils/supabase.ts
@@ -1,13 +1,26 @@
-import 'react-native-url-polyfill/auto';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_CONFIG } from '@/config';
+import 'react-native-url-polyfill/auto'
+import * as SecureStore from 'expo-secure-store'
+import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_CONFIG } from '@/config'
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key)
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value)
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key)
+  },
+}
+
 
 export const supabase = createClient(SUPABASE_CONFIG.SUPABASE_URL, SUPABASE_CONFIG.SUPABASE_ANON_KEY, {
   auth: {
-    storage: AsyncStorage,
+    storage: ExpoSecureStoreAdapter as any,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
   },
-});
+})
