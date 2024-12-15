6568a5b036514813734df073a12f81fb7d697036
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2024-12-15 22:38:24 +0530

    formate main.py

--- backend/main.py
+++ backend/main.py
@@ -7,16 +7,17 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins, or specify a list of allowed origins like ["http://example.com"]
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-#app.include_router(mqtt_router, prefix="/mqtt", tags=["mqtt"])
+# app.include_router(mqtt_router, prefix="/mqtt", tags=["mqtt"])
 app.include_router(user_routers, prefix="/user", tags=["users"])
 
 if __name__ == "__main__":
     import uvicorn
+
     # TODO: add error handling for mqtt client (if it does not starts)
-    #run_mqtt_client()
+    # run_mqtt_client()
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
