d139095fd08bb41e3e5be02233f6871b4dd7cf8e
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2024-12-17 15:02:07 +0530

    add hydration goal component to profile section
    man check hyptic feedback on slider its so good

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,7 +1,8 @@
 import { useAuth } from '@/context/authContext';
-import { Button } from '@rneui/themed';
+import Slider from '@react-native-community/slider';
 import { LinearGradient } from 'expo-linear-gradient';
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, Switch, Vibration } from 'react-native';
 
 export default function Tab() {
   const { signOut } = useAuth()
@@ -16,23 +17,79 @@ export default function Tab() {
         <Text style={styles.headerText}>Profile</Text>
       </View>
 
-      <View style={[styles.accoutCard, styles.templateCard]}>
-        <View style={styles.profileImageContainer}>
-        </View>
-        <View style={{ justifyContent: 'center', paddingLeft: 12 }}>
-          <Text style={{ color: '#fff', fontSize: 22, paddingBottom: 10, fontFamily: 'Comfortaa-Regular' }}>Salty blaze</Text>
-          <Text style={{ color: '#fff', fontSize: 14, fontFamily: 'Comfortaa-Light' }}>user1@gmail.com</Text>
+      <View>
+        <Text style={{ color: '#fff', fontSize: 22, paddingBottom: 8, paddingLeft: 15, fontFamily: 'Comfortaa-Regular' }}>
+          Account
+        </Text>
+        <View style={[styles.accoutCard, styles.templateCard]}>
+          <View style={styles.profileImageContainer}>
+          </View>
+          <View style={{ justifyContent: 'center', paddingLeft: 12 }}>
+            <Text style={{ color: '#fff', fontSize: 22, paddingBottom: 10, fontFamily: 'Comfortaa-Regular' }}>Salty blaze</Text>
+            <Text style={{ color: '#fff', fontSize: 14, fontFamily: 'Comfortaa-Light' }}>user1@gmail.com</Text>
+          </View>
         </View>
       </View>
-      <Button title='Logout' onPress={signOut}></Button>
+
+      <HydrationGoal />
     </LinearGradient>
   );
 }
 
+const HydrationGoal = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const [dailyGoal, setDailyGoal] = useState(2000);
+  const [value, setValue] = useState(0);
+
+  const handleSliderChange = (sliderValue: any) => {
+    setValue(sliderValue);
+    Vibration.vibrate(10); // Vibrates for 50ms when slider value changes
+  };
+
+  return (
+    <View style={hydrationGoalStyle.container}>
+      <Text style={hydrationGoalStyle.title}>Hydration Goal</Text>
+      <View style={hydrationGoalStyle.card}>
+        <Text style={hydrationGoalStyle.goalText}>{`${isEnabled ? dailyGoal : value} ml/day`}</Text>
+        <View style={hydrationGoalStyle.row}>
+          <Text style={[hydrationGoalStyle.rowText, { color: isEnabled ? '#fff' : '#767577' }]}>
+            Use Recommended
+          </Text>
+          <Switch
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={isEnabled ? '#1fadff' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+        </View>
+        {
+          !isEnabled && (
+            <Slider
+              style={{ flex: 1, paddingVertical: 20 }}
+              minimumValue={250}
+              maximumValue={5000}
+              step={250}
+              value={value}
+              onValueChange={handleSliderChange}
+              minimumTrackTintColor="#1E90FF"  // Color before slider thumb
+              maximumTrackTintColor="#D3D3D3"  // Color after slider thumb
+              thumbTintColor="#1E90FF"         // Thumb (circle) color
+            />
+          )
+        }
+      </View>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start',
+    gap: 30,
+
   },
   header: {
     paddingTop: 40,
@@ -56,6 +113,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
     padding: 15,
   },
+  hydrationGoalCard: {
+    justifyContent: 'flex-start',
+    height: 150,
+    marginHorizontal: 15,
+    padding: 15,
+  },
   profileImageContainer: {
     borderWidth: 1,
     height: 115,
@@ -63,3 +126,43 @@ const styles = StyleSheet.create({
     borderRadius: 100,
   }
 });
+
+
+const hydrationGoalStyle = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 22,
+    paddingBottom: 8,
+    paddingLeft: 15,
+    fontFamily: 'Comfortaa-Regular',
+  },
+  card: {
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    borderRadius: 15,
+    justifyContent: 'flex-start',
+    marginHorizontal: 15,
+    paddingVertical: 10,
+  },
+  goalText: {
+    color: '#fff',
+    fontSize: 22,
+    paddingBottom: 8,
+    paddingLeft: 15,
+    fontFamily: 'Comfortaa-Regular',
+  },
+  row: {
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderTopColor: '#fff',
+    marginHorizontal: 15,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  rowText: {
+    fontSize: 14,
+    fontFamily: 'Comfortaa-Regular',
+  },
+})
