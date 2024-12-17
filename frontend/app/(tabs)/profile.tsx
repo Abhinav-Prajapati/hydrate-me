ad776e0e8f753b770dc082289e827a17dc28c873
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2024-12-17 11:06:12 +0530

    add logout button in profile for debuging

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,9 +1,14 @@
+import { useAuth } from '@/context/authContext';
+import { Button } from '@rneui/themed';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function Tab() {
+  const { signOut } = useAuth()
+
   return (
     <View style={styles.container}>
       <Text>Profile</Text>
+      <Button title='Logout' onPress={signOut}></Button>
     </View>
   );
 }
