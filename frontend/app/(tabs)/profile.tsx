11e2781ce08548ff955ab55969d5834812e6a979
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2024-12-17 13:27:35 +0530

    add user profile card (ui only)

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,22 +1,65 @@
 import { useAuth } from '@/context/authContext';
 import { Button } from '@rneui/themed';
+import { LinearGradient } from 'expo-linear-gradient';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function Tab() {
   const { signOut } = useAuth()
-
   return (
-    <View style={styles.container}>
-      <Text>Profile</Text>
+    <LinearGradient
+      colors={['#1A5C87', '#19233e']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.container}
+    >
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Profile</Text>
+      </View>
+
+      <View style={[styles.accoutCard, styles.templateCard]}>
+        <View style={styles.profileImageContainer}>
+        </View>
+        <View style={{ justifyContent: 'center', paddingLeft: 12 }}>
+          <Text style={{ color: '#fff', fontSize: 22, paddingBottom: 10, fontFamily: 'Comfortaa-Regular' }}>Salty blaze</Text>
+          <Text style={{ color: '#fff', fontSize: 14, fontFamily: 'Comfortaa-Light' }}>user1@gmail.com</Text>
+        </View>
+      </View>
       <Button title='Logout' onPress={signOut}></Button>
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'space-between',
   },
+  header: {
+    paddingTop: 40,
+    paddingBottom: 10,
+    paddingLeft: 15,
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 22,
+    fontFamily: 'Comfortaa-Bold',
+  },
+  templateCard: {
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    borderRadius: 15,
+  },
+  accoutCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 150,
+    marginHorizontal: 15,
+    padding: 15,
+  },
+  profileImageContainer: {
+    borderWidth: 1,
+    height: 115,
+    width: 115,
+    borderRadius: 100,
+  }
 });
