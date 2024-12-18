1814cba33a7166655c092c2228ecf5251c1e2611
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2024-12-18 13:22:13 +0530

    ditch authContext provider and use zustnad auth store

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,8 +2,17 @@ import { Stack } from "expo-router";
 import { AuthProvider } from "@/context/authContext";
 import ProtectedRoute from "@/context/ProtectedRoute";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import useAuthStore from "@/store/useAuthStore";
 
 export default function RootLayout() {
+  /*
+   * fetch user session and so it is acessable to entire application
+   * */
+  const fetchSession = useAuthStore((state) => state.fetchSession);
+  useEffect(() => {
+    fetchSession();
+  }, [fetchSession]);
   return (
     <AuthProvider>
       <ProtectedRoute>
