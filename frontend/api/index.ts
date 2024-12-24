3513e1cc263a200e8773abe066b79f436ccd6922
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2024-12-24 10:03:47 +0530

    update api call function

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,16 +1,22 @@
 // @api/index.ts
-const API_BASE_URL = 'http://192.168.81.213:8000';
+const API_BASE_URL = 'http://192.168.122.213:3002';
+
 
 export const addWaterIntake = async (sensorId: string, intake: number, token: string) => {
   const encodedTime = encodeURIComponent(new Date().toLocaleString('sv-SE', { timeZoneName: 'short' }).replace(' ', 'T').slice(0, 19));
   console.log(encodedTime)
-  const response = await fetch(`${API_BASE_URL}/user/add_intake?sensor_id=${sensorId}&intake=${intake}&time=${encodedTime}`, {
+  const response = await fetch(`${API_BASE_URL}/user/add_intake`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
+      'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`,
     },
-    body: JSON.stringify({}),
+    body: JSON.stringify({
+      sensor_id: sensorId,
+      intake: intake,
+      time: encodedTime
+    }),
   });
 
   if (!response.ok) {
@@ -29,7 +35,7 @@ interface userWaterDateResponse {
 }
 
 export const getUsersWaterRelatedData = async (token: string): Promise<userWaterDateResponse> => {
-  const response = await fetch(`${API_BASE_URL}/user/get_user_water_attributes`, {
+  const response = await fetch(`${API_BASE_URL}/user/get_water_attributes`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -51,7 +57,7 @@ interface UserProfileResponse {
 }
 
 export const getUserProfile = async (token: string): Promise<UserProfileResponse> => {
-  const response = await fetch(`${API_BASE_URL}/user/get_user_profile`, {
+  const response = await fetch(`${API_BASE_URL}/user/profile`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -68,11 +74,11 @@ export const getUserProfile = async (token: string): Promise<UserProfileResponse
 export const setDailyGoal = async (token: string, dailyGaol: number) => {
   const response = await fetch(`${API_BASE_URL}/user/set_daily_goal?dailyGoal=${dailyGaol}`,
     {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         'Accept': 'application/json',
         'Authorization': `Bearer ${token}`,
-      }
+      },
     });
   if (!response.ok) {
     throw new Error(`Error: ${response.statusText}`)
